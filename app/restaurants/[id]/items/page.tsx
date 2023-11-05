bb39e0151fb5c243826eb34609e949610feaaf4d
Ihor Bobulych <ihbobuacc@gmail.com>
2023-11-05 19:52:44 +0200

    Add keys to divs

--- app/restaurants/[id]/items/page.tsx
+++ app/restaurants/[id]/items/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     <div>
       {menuItems.map(
         (menuItem) => (
-          <div className="flex flex-col w-72 h-fit p-5">
+          <div key={menuItem.id} className="flex flex-col w-72 h-fit p-5">
             <h1>{menuItem.name}</h1>
             <Image src={menuItem.pizza_image_url} alt={menuItem.name} width={190} height={190} />
             <p>{menuItem.topping.join(' ')}</p>
