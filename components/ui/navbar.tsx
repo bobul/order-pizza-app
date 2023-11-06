69864fcbaae8b8200e5fece98e85ac13339e32b9
Ihor Bobulych <ihbobuacc@gmail.com>
2023-11-06 22:00:27 +0200

    Wrap logo into link

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -9,20 +9,24 @@ import CartItems from "@/components/ui/cart-items";
 export default function Navbar() {
     return (
         <nav className="h-16 bg-orange-500 flex flex-auto justify-around items-center">
+          <Link href="/">
             <Image
-                src="/logo.svg"
-                alt="logo"
-                width={120}
-                height={80}
-                className="hidden md:block"
+              src="/logo.svg"
+              alt="logo"
+              width={120}
+              height={80}
+              className="hidden md:block"
             />
+          </Link>
+          <Link href="/">
             <Image
-                src="/logo.svg"
-                alt="logo"
-                width={80}
-                height={40}
-                className="block md:hidden"
+              src="/logo.svg"
+              alt="logo"
+              width={80}
+              height={40}
+              className="block md:hidden"
             />
+          </Link>
             <div className="flex justify-around md:w-1/4 w-1/2">
                 <Link href={`/restaurants/book`}>
                     <p className="text-base font-bold text-gray-50">Book</p>
